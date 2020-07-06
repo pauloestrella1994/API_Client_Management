0e80a0e00fffe954974df3eea64b1144fee89bde
Paulo Netto <pauloestrella1994@gmail.com>
2020-07-06 14:07:13 -0300

    [11] - Changing models for manytomany relationships

--- Employees/models.py
+++ Employees/models.py
@@ -5,8 +5,8 @@ from Departament.models import Departament
 
 class Employee(models.Model):
     name = models.CharField(max_length=100, help_text="Name of the employee")
-    company = models.OneToOneField(Company, on_delete=models.SET_NULL,blank=True, null=True)
-    departament = models.OneToOneField(Departament, on_delete=models.SET_NULL, blank=True, null=True)
+    company = models.ManyToManyField(Company, blank=True, null=True)
+    departament = models.ManyToManyField(Departament, blank=True, null=True)
 
     def __str__(self):
         return self.name
